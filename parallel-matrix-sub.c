e1e2a77c986fd3a5206d34e7813259debb5d633b
AwaisHanif10 <22ntucs1190awaishanif@gmail.com>
2025-03-11 00:26:48 +0500

    Add function to subtract two matrices using OpenMP

--- parallel-matrix-sub.c
+++ parallel-matrix-sub.c
@@ -16,3 +16,14 @@ void initializeMatrix(int matrix[ROWS][COLS]) {
         }
     }
 }
+
+// Function to subtract two matrices using OpenMP parallelization
+void subtractMatrices(int A[ROWS][COLS], int B[ROWS][COLS], int result[ROWS][COLS]) {
+    // Parallelizing the matrix subtraction using OpenMP
+    #pragma omp parallel for collapse(2) shared(A, B, result) default(none)
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            result[i][j] = A[i][j] - B[i][j];
+        }
+    }
+}
